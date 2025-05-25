a29e123ec6a06b3b97518a25b471016d22a1f9f3
iwin siju <77713114+XOUT1@users.noreply.github.com>
2025-05-25 22:11:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+let planeImage;
+let enemyImages = [];
+
+
+
+function preload() {
+  // Load player image
+  planeImage = loadImage('assets/plane.png');
+
+  // Load enemy images
+  enemyImages[0] = loadImage('assets/enemy1.png');
+  enemyImages[1] = loadImage('assets/enemy2.png');
+  enemyImages[2] = loadImage('assets/enemy3.png');
+
+  // load sound
+}
 
 
 let currentScene;
-
 
 function setup() {
   createCanvas(1200, 600);
