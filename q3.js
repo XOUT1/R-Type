4762c61e06c58c90cecacbb0a3cfe788a490f7be
iwin siju <77713114+XOUT1@users.noreply.github.com>
2025-05-25 21:58:25 +0300

    added some more features!

--- q3.js
+++ q3.js
@@ -1,4 +1,5 @@
 let gamePlayer;
+let gameBgVideo;
 let bullets = [];
 let enemies = [];
 let lastEnemySpawnTime = 0;
@@ -11,11 +12,24 @@ class GameScene {
     enemies = [];
     score = 0;
     lastEnemySpawnTime = millis();
+
+    gameBgVideo = createVideo('./assets/stars_bg.mp4');
+    gameBgVideo.size(width, height);
+    gameBgVideo.loop();
+    gameBgVideo.volume(0);
+    gameBgVideo.hide();
+
+    this.popupText = "";
+    this.popupTimer = 0;
+  }
+
+  showHitPopup() {
+    this.popupText = "Hit! -100";
+    this.popupTimer = millis();
   }
 
   draw() {
-    background(0);
-    this.drawStars(); // optional effect
+    image(gameBgVideo, 0, 0, width, height);
 
     fill(255);
     textSize(24);
@@ -24,6 +38,16 @@ class GameScene {
 
     gamePlayer.update();
     gamePlayer.display();
+
+    if (this.popupText && millis() - this.popupTimer < 1500) {
+      fill(255, 0, 0);
+      stroke(0);
+      strokeWeight(4);
+      textSize(36);
+      textAlign(CENTER, CENTER);
+      text(this.popupText, width / 2, height / 2);
+      noStroke();
+    }
 
     // Spawn enemies
     if (millis() - lastEnemySpawnTime > 1500) {
@@ -40,27 +64,37 @@ class GameScene {
       }
     }
 
-    // Update and draw enemies
+    // Update and draw enemies + check for collisions
     for (let i = enemies.length - 1; i >= 0; i--) {
-      drawSprite(enemies[i].sprite);
-      if (enemies[i].isOffScreen()) {
-        enemies[i].destroy();
+      let enemy = enemies[i];
+      drawSprite(enemy.sprite);
+
+      // Check enemy hits player
+      if (enemy.sprite.overlap(gamePlayer.sprite)) {
+        score -= 100;
+        this.showHitPopup();
+        enemy.destroy();
         enemies.splice(i, 1);
+        continue;
       }
-    }
 
-    // Check bullet-enemy collisions
-    for (let i = enemies.length - 1; i >= 0; i--) {
+      // Check bullet-enemy collisions
       for (let j = bullets.length - 1; j >= 0; j--) {
-        if (bullets[j].sprite.overlap(enemies[i].sprite)) {
-        score += 100;
+        if (bullets[j].sprite.overlap(enemy.sprite)) {
+          score += 100;
 
-        bullets[j].destroy();
-        enemies[i].destroy();
-        bullets.splice(j, 1);
-        enemies.splice(i, 1);
-        break;
+          bullets[j].destroy();
+          enemy.destroy();
+          bullets.splice(j, 1);
+          enemies.splice(i, 1);
+          break;
         }
+      }
+
+      // Remove enemy if off screen
+      if (enemy && enemy.isOffScreen()) {
+        enemy.destroy();
+        enemies.splice(i, 1);
       }
     }
   }
@@ -68,14 +102,6 @@ class GameScene {
   keyPressed() {
     if (key === ' ') {
       gamePlayer.shoot(bullets);
-    }
-  }
-
-  drawStars() {
-    // Simple random starfield
-    for (let i = 0; i < 100; i++) {
-      stroke(255);
-      point(random(width), random(height));
     }
   }
 }
